1a4e4194f6023dd2d33de774a23698ec08858103
Di <inferneris@gmail.com>
2022-02-15 22:02:03 +0300

    add practice #1

---
+++ practice 1.py
@@ -0,0 +1,12 @@
+number_1 = 5 * 3
+print(number_1)
+number_2 = 15 / 3
+print(number_2)
+number_3 = 15 // 2
+print(number_3)
+number_4 = 15 ** 2
+print(number_4)
+print(type(number_1))
+print(type(number_2))
+print(type(number_3))
+print(type(number_4))
