8593d72d0073081d4d86fc3239505ed930b23e62
Di <inferneris@gmail.com>
2022-02-15 14:34:56 +0300

    fixed bugs

--- practice 1.py
+++ practice 1.py
@@ -1,13 +1,9 @@
 duration = int(input('Введите время в секундах: '))
-hour = duration // 3600
-day = duration // 8600
-minute = (duration - hour * 3600)// 60
-second = duration % (24 * 3600)
-second_duration = duration - (hour * 3600 + minute * 60)
-print (second, 'сек')
-print(minute, 'мин' ,second_duration, 'сек')
-print(hour, 'час' ,minute, 'мин' ,second_duration, 'сек')
-print(day, 'дн' ,hour, 'час' ,minute, 'мин' ,second_duration, 'сек')
+day = duration // 86400
+hour = (duration - day * 86400) // 3600
+minute = ((duration - day * 86400) - hour * 3600) // 60
+second = duration - day * 86400 - hour * 3600 - minute * 60
+print(day, 'дн' ,hour, 'час' ,minute, 'мин' ,second, 'сек')
 
 
 
