2d2b6bc6b4abac7edf40d0de09f9d1d0a09f8942
Granautumn <98841938+Granautumn@users.noreply.github.com>
2022-02-17 18:46:12 +0300

    Update practice 3.py

--- practice 3.py
+++ practice 3.py
@@ -1,10 +1,10 @@
 percent = input('Введите процент скидки: ')
-for percent in range(1, 101):
-    if 4 < percent % 100 <= 20:
+for percent in range(1, 101, 2):
+    if 4 < percent % 102 <= 20:
         print(percent, 'процентов')
-    elif precent % 10 == 1:
+    elif percent % 10 == 1:
         print(percent, 'процент')
-    elif 1 < percentt % 10 < 5:
+    elif 1 < percent % 10 < 5:
         print(percent, 'процента')
     else:
         print(percent, 'процентов')
